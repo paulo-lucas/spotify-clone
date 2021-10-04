7b428ba566646d750478f06b67625d9e419291e0
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2021-10-04 19:15:23 -0300

    adding all iconbuttons to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,24 +1,23 @@
 import React from "react"
 import styled from "styled-components"
-import {
-  MdHome,
-  MdPerson,
-  MdPlaylistPlay,
-  MdMusicNote,
-  MdAlbum,
-} from 'react-icons/md'
+import { RiHome5Fill } from "react-icons/ri"
+import { FiSearch } from "react-icons/fi"
 import { ImBooks } from "react-icons/im"
-import { FiSearch, FiClock } from 'react-icons/fi'
-import { FaHeart } from 'react-icons/fa'
-import { RiHomeFill, RiHome5Fill, RiHome2Fill } from "react-icons/ri"
+import { AiFillPlusSquare } from "react-icons/ai"
+import { BsFillBookmarkHeartFill } from "react-icons/bs"
 
 import SidebarIconButton from "components/SidebarIconButton"
 
 const Sidebar = () => {
   return <Bar>
-    <SidebarIconButton icon={RiHome5Fill} label="Home" className="selected" />
-    <SidebarIconButton icon={FiSearch} label="Search" />
-    <SidebarIconButton icon={ImBooks} label="Your Library" />
+    <MainButtonsWrapper>
+      <SidebarIconButton icon={RiHome5Fill} label="Home" className="selected" />
+      <SidebarIconButton icon={FiSearch} label="Search" />
+      <SidebarIconButton icon={ImBooks} label="Your Library" />
+    </MainButtonsWrapper>
+
+    <SidebarIconButton icon={AiFillPlusSquare} label="Create Playlist" />
+    <SidebarIconButton icon={BsFillBookmarkHeartFill} label="Liked Songs" />
   </Bar>
 }
 
@@ -26,7 +25,11 @@ const Bar = styled.nav`
   background-color: ${({ theme }) => theme.colors.dark};
   width: 240px;
   height: 100%;
-  padding: 48px 8px;
+  padding: 24px 8px;
+`
+
+const MainButtonsWrapper = styled.div`
+  padding: 24px 0;
 `
 
 export default Sidebar
